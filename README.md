851dd1f66ba67f7136506004218396bb6a7fe8e8
Boni García <boni.garcia@uc3m.es>
2021-03-03 18:16:44 +0100

    Include Jitsi test

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
 [![License badge](https://img.shields.io/badge/license-Apache2-green.svg)](http://www.apache.org/licenses/LICENSE-2.0)
 [![Twitter](https://img.shields.io/badge/follow-@boni_gg-green.svg)](https://twitter.com/boni_gg)
 
-# selenium-jupiter-webrtc [![][Logo]][GitHub Repository]
+# Selenium-Jupiter-WebRTC [![][Logo]][GitHub Repository]
 
-This project contains several tests aimed to assess online WebRTC applications with [selenium-jupiter], namely:
+This project contains several tests aimed to assess online WebRTC applications with [Selenium-Jupiter], namely:
 
+* [Jitsi]
 * [OpenVidu]
 * [Talky]
 
 ## About
 
-selenium-jupiter-webrtc (Copyright &copy; 2018-2021) is a project by [Boni Garcia] licensed under [Apache 2.0 License].
+Selenium-Jupiter-WebRTC (Copyright &copy; 2018-2021) is a project by [Boni Garcia] licensed under [Apache 2.0 License].
 
 [Apache 2.0 License]: http://www.apache.org/licenses/LICENSE-2.0
 [Boni Garcia]: http://bonigarcia.github.io/
 [GitHub Repository]: https://github.com/bonigarcia/selenium-jupiter-webrtc
+[Jitsi]: https://meet.jit.si/
 [Logo]: http://bonigarcia.github.io/img/selenium-jupiter.png
 [OpenVidu]: http://openvidu.io/
-[selenium-jupiter]: https://github.com/bonigarcia/selenium-jupiter
+[Selenium-Jupiter]: https://github.com/bonigarcia/selenium-jupiter
 [Talky]: https://talky.io/
---
+++ src/test/java/io/github/bonigarcia/webrtc/test/JitsiTest.java
@@ -0,0 +1,106 @@
+/*
+ * (C) Copyright 2021 Boni Garcia (http://bonigarcia.github.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.github.bonigarcia.webrtc.test;
+
+import static io.github.bonigarcia.seljup.BrowserType.CHROME;
+import static java.util.UUID.randomUUID;
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.seljup.Arguments;
+import io.github.bonigarcia.seljup.DockerBrowser;
+import io.github.bonigarcia.webrtc.base.WebRtcBase;
+
+class JitsiTest extends WebRtcBase {
+
+    static final String APP_URL = "https://meet.jit.si/";
+
+    @Test
+    void testJitsy(
+            @Arguments({ "--use-fake-device-for-media-stream",
+                    "--use-fake-ui-for-media-stream" }) ChromeDriver driver,
+            @Arguments({ "--use-fake-device-for-media-stream",
+                    "--use-fake-ui-for-media-stream" })
+            @DockerBrowser(type = CHROME, size = NUM_VIEWERS) List<WebDriver> driverList)
+            throws Exception {
+        log.debug("Benchmarking WebRTC room at {}", APP_URL);
+
+        // Open webrtc-internals in second tab and return to first one
+        openWebRtcInternals(driver);
+
+        // Presenter
+        log.debug("Entering {}", driver);
+        driver.get(APP_URL);
+        String roomName = randomUUID().toString();
+        createRoom(driver, roomName, "presenter");
+        String sessionUrl = getCurrentUrl(driver, APP_URL);
+
+        // Viewers
+        int numViewer = 1;
+        for (WebDriver wd : driverList) {
+            log.debug("Waiting {} seconds for new browser", BROWSERS_RATE_SEC);
+            waitSeconds(BROWSERS_RATE_SEC);
+
+            log.debug("Entering {}", wd);
+            String userName = "viewer" + numViewer;
+            execute(() -> {
+                wd.get(sessionUrl);
+                joinCall(wd, userName);
+            });
+            numViewer++;
+        }
+
+        // Wait session time
+        log.debug("Waiting {} seconds with all participants", SESSION_TIME_SEC);
+        waitSeconds(SESSION_TIME_SEC);
+
+        // Download WebRC stats
+        downloadStats(driver);
+    }
+
+    void createRoom(WebDriver driver, String roomName, String userName) {
+        By sessionSelector = By.id("enter_room_field");
+        driver.findElement(sessionSelector).clear();
+        driver.findElement(sessionSelector).sendKeys(roomName);
+
+        By joinSelector = By.id("enter_room_button");
+        driver.findElement(joinSelector).click();
+
+        joinCall(driver, userName);
+    }
+
+    void joinCall(WebDriver driver, String userName) {
+        By userSelector = By.cssSelector(
+                "#lobby-screen > div.content > div.prejoin-input-area-container > div > input");
+
+        driver.findElement(userSelector).clear();
+        driver.findElement(userSelector).sendKeys(userName);
+
+        By joinCallSelector = By.className("action-btn");
+        WebDriverWait wait = new WebDriverWait(driver, 5); // seconds
+        wait.until(elementToBeClickable(joinCallSelector));
+        driver.findElement(joinCallSelector).click();
+    }
+
+}
